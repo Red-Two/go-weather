b4b1e4dbe00025736779f50d1c75b9fe1fc03c45
Alex Russell <holcron@Alexs-MBP.home>
2017-03-23 22:38:36 -0400

    Modified

--- main.go
+++ main.go
@@ -5,17 +5,28 @@ import (
   //"flag"
   //"log"
   "net/http"
-  //"strings"
+  "strings"
   //"time"
-  "fmt"
 )
 
 func main() {
-  fmt.Println(query("Rochester"))
+  http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+    city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+    data, err := query(city)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    json.NewEncoder(w).Encode(data)
+  })
+
+  http.ListenAndServe(":8080", nil)
 }
 
 func query(city string) (weatherData,error) {
-  apiKey := "63c43a8187390586d5dbb89fe9567a66"
+  apiKey := ""
   resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiKey + "&q=" + city)
   if err != nil {
     return weatherData{}, err
@@ -29,9 +40,37 @@ func query(city string) (weatherData,error) {
   return d, nil
 }
 
+type weatherProvider interface {
+  temperature(city string) (float64, error) //kelvin
+}
+
 type weatherData struct {
   Name string `json:"name"`
   Main struct {
     Kelvin float64 `json:"temp"`
   } `json:"main"`
+}
+
+type openWeatherMap struct{}
+
+func (w openWeatherMap) temperature(city string) (float64, error) {
+  apiKey := ""
+  resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiKey + "&q=" + city)
+  if err != nil {
+    return 0, err
+  }
+  defer resp.Body.Close()
+
+  var d struct {
+    Main struct {
+      Kelvin float64 `json:"temp"`
+    } `json:"main"`
+  }
+
+  if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+    return 0, err
+  }
+
+  log.Pringf("openWeatherMap: %s: %.2f", city, d.Main.Kelvin)
+  return d.Main.Kelvin, nil
 }
